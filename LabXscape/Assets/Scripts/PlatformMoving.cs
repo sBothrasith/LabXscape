11dc41b65e82e137848e1c08e6e761c3ae098a04
bunchhean.lu <bunchheanlu@gmail.com>
2023-02-05 21:38:40 +0700

    Update Moving Platform

---
+++ LabXscape/Assets/Scripts/PlatformMoving.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformMoving : MonoBehaviour
+{ 
+    [SerializeField] private GameObject[] waypoints;
+    private int currentPointIndex = 0;
+
+    [SerializeField] private float speed = 2f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector2.Distance(waypoints[currentPointIndex].transform.position, transform.position) < .1f){
+            currentPointIndex++;
+            if (currentPointIndex >= waypoints.Length)
+            {
+                currentPointIndex = 0;
+            }
+        }
+
+        transform.position = Vector2.MoveTowards(transform.position, waypoints[currentPointIndex].transform.position, Time.deltaTime * speed);
+    }
+
+    private void onCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.position.y > transform.position.y)
+        {
+            collision.transform.SetParent(transform);
+        }
+    }
+
+    private void onCollisionExit2D(Collision2D collision)
+    {
+        collision.transform.SetParent(null);
+    }
+
+}
