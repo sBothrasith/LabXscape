e0127784adcc8e714b2cae64580e625c575384a7
sBothrasith <somouthbothrasith1@gmail.com>
2023-03-02 14:46:16 +0700

    UI

---
+++ LabXscape/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Events;
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GamePaused = false;
+
+    public UnityEvent PlayerPause;
+    public UnityEvent PlayerResume;
+
+    public GameObject pauseIcon;
+    public GameObject pauseMenuUI;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            if(GamePaused) {
+                Resume();
+            }
+            else {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume() {
+        pauseMenuUI.SetActive(false);
+        pauseIcon.SetActive(true);
+        Time.timeScale = 1f;
+        GamePaused = false;
+        PlayerResume.Invoke();
+    }
+
+    public void Pause() {
+        pauseMenuUI.SetActive(true);
+        pauseIcon.SetActive(false);
+        Time.timeScale = 0f;
+        GamePaused = true;
+        PlayerPause.Invoke();
+    }
+
+    public void LoadMenu() {
+        SceneManager.LoadScene(0);
+    }
+
+
+}
