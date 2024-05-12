d4227d039fd2061f8fb1b57d183aacd3ca8abf5b
bunchhean.lu <bunchheanlu@gmail.com>
2024-05-12 21:47:36 +0700

    Update Light mechs Stage2

--- LabXscape/Assets/Scripts/Stage_2/ColorChange.cs
+++ LabXscape/Assets/Scripts/Stage_2/ColorChange.cs
@@ -5,42 +5,33 @@ using UnityEngine.Rendering.Universal;
 
 public class ColorChange : MonoBehaviour
 {
-    public PuzzleManager puzzle;
+    public WordPuzzleSolveManager worldPuzzleManager;
 
     public GameObject LightA, LightB, LightC;
-    public Light2D lightChangeA;
-    public Light2D lightChangeB;
-    public Light2D lightChangeC;
+    private Light2D lightChangeA;
+	private Light2D lightChangeB;
+	private Light2D lightChangeC;
 
     Color colorRed = Color.red;
     Color colorGreen = Color.green;
 
     private void Start()
     {
-        lightChangeA = LightA.GetComponent<Light2D>();
-        lightChangeB = LightB.GetComponent<Light2D>();
-        lightChangeC = LightC.GetComponent<Light2D>();
+        LightA.GetComponent<Light2D>().color = colorRed;
+        LightB.GetComponent<Light2D>().color = colorRed;
+        LightC.GetComponent<Light2D>().color = colorRed;
 
-    }
+	}
     // Update is called once per frame
     void Update()
     {
-        lightChangeA.color = colorRed;
-        lightChangeB.color = colorRed;
-        lightChangeC.color = colorRed;
-        if (puzzle.lightA_Active)
-        {
-            lightChangeA.color = colorGreen;
-        }
-        if (puzzle.lightA_Active && puzzle.lightB_Active)
-        {
-            lightChangeB.color = colorGreen;
-        }
-        if (puzzle.lightA_Active && puzzle.lightB_Active && puzzle.lightC_Active)
-        {
-            lightChangeC.color = colorGreen;
-        }
-        
+
+        if (worldPuzzleManager.solved)
+        { 
+			LightA.GetComponent<Light2D>().color = colorGreen;
+			LightB.GetComponent<Light2D>().color = colorGreen;
+			LightC.GetComponent<Light2D>().color = colorGreen;
+		}
     }
 
             
